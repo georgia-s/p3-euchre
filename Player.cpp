faa46bfb28f40286a97c3d648bd48085d95b6493
Anastasia Kazanas <akazanas@umich.edu>
2023-02-15 17:48:47 -0500

    Player

--- Player.cpp
+++ Player.cpp
@@ -4,171 +4,263 @@
 //
 //  Created by Anastasia Kazanas on 2/12/23.
 //
-
+#include "Card.h"
+#include <string>
+#include <vector>
 #include "Player.h"
 #include <algorithm>
 #include <cassert>
+#include <vector>
 
-class Simple :public Player {
-private:
-    std::string nameP;
-    std::vector<Card> hand{};
-
+class SimplePlayer : public Player {
 public:
-    Simple(std::string namePlayer) : nameP(namePlayer) {}
+    // defualt constructor
+    SimplePlayer(std::string name) : simple_name(name) {}
+   
     //EFFECTS returns player's name
-    virtual const std::string & get_name() const {
-        return nameP;
+    const std::string & get_name() const{
+        return simple_name;
     }
-
+    
     //REQUIRES player has less than MAX_HAND_SIZE cards
-    //EFFECTS  adds Card c to Player's hand
-    virtual void add_card(const Card& c) {
-        if (hand.size() < MAX_HAND_SIZE) {
-            hand.push_back(c);
+    //EFFECTS adds Card c to Player's hand
+    void add_card(const Card &c){
+        if (simple_hand.size() < MAX_HAND_SIZE) {
+            simple_hand.push_back(c);
         }
-        std::sort(hand.begin(), hand.end());
+        std::sort(simple_hand.begin(), simple_hand.end());
     }
-
-    //REQUIRES round is 1 or 2
-    //MODIFIES order_up_suit
-    //EFFECTS If Player wishes to order up a trump suit then return true and
-    //  change order_up_suit to desired suit.  If Player wishes to pass, then do
-    //  not modify order_up_suit and return false.
-    virtual bool make_trump(const Card& upcard, bool is_dealer,int round, std::string& order_up_suit) const {
-        int trumps = 0;
-        int size = static_cast <int>(hand.size());
-        if (round == 1) {
-            for (int i = 0; i < size; ++i) {
-                if (hand[i].is_face_or_ace() && hand[i].is_trump(upcard.get_suit())) {
-                    ++trumps;
-                }
-            }
-            if (trumps >= 2) {
-                order_up_suit = upcard.get_suit();
-                return true;
-            }
+    
+    bool make_trump(const Card &upcard, bool is_dealer,
+                            int round, Suit &order_up_suit) const{
+        for (size_t i=0; i < simple_hand.size(); ++i) {
+            std::cout << "Human player " << simple_name << "'s hand: "
+                 << "[" << i << "] " << simple_hand[i] << "\n";
         }
-        if (round == 2) {
-            Suit nextSuit = Suit_next(upcard.get_suit());
-            for (int i = 0; i < size; ++i) {
-                if (hand[i].is_face_or_ace() && hand[i].is_trump(nextSuit)) {
-                    ++trumps;
-                }
-            }
-            if (trumps >= 1) {
-                order_up_suit = nextSuit;
-                return true;
-            }
-            if (is_dealer) {
-                order_up_suit = nextSuit;
-            }
+        std::cout << "Human player " << simple_name
+                  << ", please enter a suit, or \"pass\":\n";
+        
+        std::string decision;
+        std::cin >> decision;
+        
+        if (decision != "pass") {
+            order_up_suit = string_to_suit(decision);
+            return true;
         }
-        return false;
+        else {
+            return false;
+        }
     }
-
-    //REQUIRES Player has at least one card
-    //EFFECTS  Player adds one card to hand and removes one card from hand.
-    virtual void add_and_discard(const Card &upcard) {
+    void add_and_discard(const Card &upcard) {
         Suit trump = upcard.get_suit();
         int lowest = 0;
-        int size = static_cast <int>(hand.size());
-        hand.push_back(upcard);
-        std::sort(hand.begin(), hand.end());
-        for (int i = 0; i < size; ++i) {
-            if (Card_less(hand[i], hand[lowest], trump)) {
-                lowest = i;
+        Card min = simple_hand[0];
+        simple_hand.push_back(upcard);
+        for(size_t i = 0; i < simple_hand.size(); i++){
+            if(Card_less(simple_hand[i], min, trump)){
+                min = simple_hand[i];
+                lowest = (int)i;
             }
         }
-        hand.erase((hand.begin() + lowest));
+        simple_hand.erase(simple_hand.begin() + lowest);
+        add_card(upcard);
     }
-
+    
+    
     //REQUIRES Player has at least one card
     //EFFECTS  Leads one Card from Player's hand according to their strategy
     //  "Lead" means to play the first Card in a trick.  The card
     //  is removed the player's hand.
-    virtual Card lead_card(Suit trump){
-        std::sort(hand.begin(), hand.end());
-        int size = static_cast <int>(hand.size());
-        int highest = 3;
-        bool allTrump = true;
-        for (int i = 0; i < size; ++i) {
-            if (hand[i].get_suit() != trump) {
-                // for--> of all trump --> if 1not trump
-                allTrump = false;
+    //need to do
+    Card lead_card(Suit trump) {
+        Card max;
+        int start = 0;
+        while (simple_hand[start].get_suit() == trump) {
+            start++;
+        }
+        if (start != 0) {
+            max = simple_hand[start];
+        }
+        int trump_count = 0;
+        for (int i = 0; i < simple_hand.size(); i++) {
+            if (simple_hand[i].get_suit() != trump) {
+                if (simple_hand[i] > max) {
+                    max = simple_hand[i];
+                    trump_count++;
+                }
+            // all trump cards
             }
         }
-        for (int i = 0; i < size; ++i) {
-            if (allTrump == true && Card_less(hand[highest], hand[i], trump)) {
-                highest = i;
-            }
-            if (allTrump == false) {
-                if (hand[i].get_suit() != trump &&Card_less(hand[highest], hand[i], trump)) {
-                    highest = i;
+        if(trump_count == 0) {
+            if(start == simple_hand.size()) {
+                max = simple_hand[0];
+                for (int i = 0; i < simple_hand.size(); i++) {
+                    if (simple_hand[i] > max) {
+                        simple_hand[i] = max;
+                    }
                 }
             }
         }
-        Card H = hand[highest];hand.erase((hand.begin() + highest));
-        return H;
+        return max;
     }
-
+    
     //REQUIRES Player has at least one card
     //EFFECTS  Plays one Card from Player's hand according to their strategy.
     //  The card is removed from the player's hand.
-    virtual Card play_card(const Card &led_card, Suit trump){
-        int highest = 4;
-        int size = static_cast <int>(hand.size());
-        int lowest = 0;
-        std::sort(hand.begin(), hand.end());
-        for (int i = 0; i < size; ++i) {
-            if (hand[i].get_suit() == led_card.get_suit()) {
-                if (Card_less(hand[highest], hand[i], led_card, trump)) {
-                    highest = i;
-                }
+    Card play_card(const Card &led_card, Suit trump){
+        Card max = simple_hand[0];
+        bool same_suit = false;
+        for (int i = 0; i < simple_hand.size(); i++) {
+            if (simple_hand[i].get_suit() == led_card.get_suit()) {
+                same_suit = true;
             }
-            Card H = hand[i];hand.erase((hand.begin() + i));
-            return H;
         }
-        for (int i = 0; i < size; ++i) {
-            if (hand[i].get_suit() != led_card.get_suit()) {
-                if (Card_less(hand[i], hand[lowest], led_card, trump)) {
-                    lowest = i;
+        if (same_suit){
+            for (int i = 0; i < simple_hand.size(); i++) {
+                if (simple_hand[i].get_suit() == led_card.get_suit()) {
+                    if (simple_hand[i] > max) {
+                        simple_hand[i] = max;
+                    }
                 }
+                simple_hand.erase(simple_hand.begin() + i);
             }
-            Card L = hand[i];hand.erase((hand.begin() + i));
-            return L;
+        } else {
+            for (int i = 0; i < simple_hand.size(); i++) {
+                if (simple_hand[i] > max) {
+                    simple_hand[i] = max;
+                }
+                simple_hand.erase(simple_hand.begin() + i);
+            }
         }
-        return led_card;
-    }
-};
-    
-class Human :public Player {
-private:
-    std::string nameP;
-    std::vector<Card> hand;
         
-public:
-    Human(std::string namePlayer) : nameP(namePlayer) {}
-    //EFFECTS: Returns a pointer to a player with the given name and strategy
-    //To create an object that won't go out of scope when the function returns,
-    //use "return new Simple(name)" or "return new Human(name)"
-    //Don't forget to call "delete" on each Player* after the game is over
-    Player * Player_factory(const std::string &name, const std::string &strategy) {
-        if(strategy.compare("Simple") == 0){
-            //return new Simple(name);
-        }
-        if(strategy.compare("Human") == 0){
-            //return new Human(name);
-        }
-        std::cerr << "Invalid strategy" << std::endl;
-        assert(false);
-        exit(1);
+        return max;
     }
 
-    //EFFECTS: Prints player's name to os
-    
-    friend std::ostream & operator<<(std::ostream &os, const Player &p) {
-        os << p.get_name();
-        return os;
-    }
+    private:
+        std::vector <Card> simple_hand;
+        std::string simple_name;
 };
+    
+class HumanPlayer : public Player {
+public:
+    // default constructor
+    HumanPlayer(std::string name) : human_name(name) {}
+   
+    //EFFECTS returns player's name
+    const std::string & get_name() const{
+        return human_name;
+    }
+    
+    void add_card(const Card &c){
+        if (human_hand.size() < MAX_HAND_SIZE) {
+            human_hand.push_back(c);
+        }
+        std::sort(human_hand.begin(), human_hand.end());
+    }
+   
+    // need to make specific to human
+    //EFFECTS If Player wishes to order up a trump suit then return true and
+    //change order_up_suit to desired suit.If Player wishes to pass, then do
+    //not modify order_up_suit and return false.
+    bool make_trump(const Card &upcard, bool is_dealer,
+                            int round, Suit &order_up_suit) const{
+        for (size_t i=0; i < human_hand.size(); ++i) {
+            std::cout << "Human player " << human_name << "'s hand: "
+                 << "[" << i << "] " << human_hand[i] << "\n";
+        }
+        std::cout << "Human player " << human_name
+                  << ", please enter a suit, or \"pass\":\n";
+        
+        std::string decision;
+        std::cin >> decision;
+        
+        if (decision != "pass") {
+            order_up_suit = string_to_suit(decision);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    void add_and_discard(const Card &upcard) {
+        Suit trump = upcard.get_suit();
+        int lowest = 0;
+        Card min = human_hand[0];
+        human_hand.push_back(upcard);
+        for(size_t i = 0; i < human_hand.size(); i++){
+            if(Card_less(human_hand[i], min, trump)){
+                min = human_hand[i];
+                lowest = (int)i;
+            }
+        }
+        human_hand.erase(human_hand.begin() + lowest);
+        add_card(upcard);
+    }
+    
+    // need to do
+    Card lead_card(Suit trump) {
+        for (size_t i=0; i < human_hand.size(); ++i) {
+            std::cout << "Human player " << human_name << "'s hand: "
+            << "[" << i << "] " << human_hand[i] << "\n";
+        }
+        std::cout << "Human player " << human_name << ", please select a card:\n";
+        Rank rank;
+        std::string junk;
+        Suit suit;
+        std::cin >> rank >> junk >> suit;
+        Card decision(rank, suit);
+        return decision;
+    }
+
+    
+    // need to do
+    Card play_card(const Card &led_card, Suit trump){
+        Card max = human_hand[0];
+        bool same_suit = false;
+        for (int i = 0; i < human_hand.size(); i++) {
+            if (human_hand[i].get_suit() == led_card.get_suit()) {
+                same_suit = true;
+            }
+        }
+        if (same_suit){
+            for (int i = 0; i < human_hand.size(); i++) {
+                if (human_hand[i].get_suit() == led_card.get_suit()) {
+                    if (human_hand[i] > max) {
+                        human_hand[i] = max;
+                    }
+                }
+                human_hand.erase(human_hand.begin() + i);
+            }
+        } else {
+            for (int i = 0; i < human_hand.size(); i++) {
+                if (human_hand[i] > max) {
+                    human_hand[i] = max;
+                }
+                human_hand.erase(human_hand.begin() + i);
+            }
+        }
+        
+        return max;
+    }
+    
+    private:
+    std::vector <Card> human_hand;
+    std::string human_name;
+};
+
+Player * Player_factory(const std::string &name, const std::string &strategy){
+    if(strategy == "Simple"){
+        return new SimplePlayer(name);
+    }
+    if(strategy == "Human"){
+        return new HumanPlayer(name);
+    }
+    assert(false);
+    return nullptr;
+}
+//EFFECTS: Prints player's name to os
+std::ostream & operator<<(std::ostream &os, const Player &p){
+    os << p.get_name();
+    return os;
+}
