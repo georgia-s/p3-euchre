19dafbfce5a237b650c66db7b73911e37e484850
georgia-s <georgias@umich.edu>
2023-02-15 20:44:09 -0500

    Euchre.cpp

--- euchre.cpp
+++ euchre.cpp
@@ -1,7 +1,15 @@
 // Project UID 1d9f47bfc76643019cfbf037641defe1
 #include <iostream>
 using namespace std;
-
+//To compile and run a Euchre game with four players.
+    //make euchre.exe
+    // ./euchre.exe pack.in noshuffle 1 Adi Simple Barbara Simple Chi-Chih Simple Dabbala Simple
 int main() {
-  cout << "Hello World!\n";
+    // Assume pack_filename is a variable containing
+// the specified pack filename from argv
+cout << "Error opening " << pack_filename << endl;
+  cout << "Usage: euchre.exe PACK_FILENAME [shuffle|noshuffle] "
+     << "POINTS_TO_WIN NAME1 TYPE1 NAME2 TYPE2 NAME3 TYPE3 "
+     << "NAME4 TYPE4" << endl;
+
 }
